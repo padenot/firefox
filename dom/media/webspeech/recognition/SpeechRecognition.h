8c3b2551e409fbaa4ad8997dc57de4d03a1a5a68
Paul Adenot <paul@paul.cx>
2025-12-08 15:15:57 +0100

    Bug 1940906 - Fix include guards in dom/media/webspeech. r?pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D275661

--- dom/media/webspeech/recognition/SpeechRecognition.h
+++ dom/media/webspeech/recognition/SpeechRecognition.h
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_SpeechRecognition_h
-#define mozilla_dom_SpeechRecognition_h
+#ifndef DOM_MEDIA_WEBSPEECH_RECOGNITION_SPEECHRECOGNITION_H_
+#define DOM_MEDIA_WEBSPEECH_RECOGNITION_SPEECHRECOGNITION_H_
 
 #include "DOMMediaStream.h"
 #include "SpeechGrammarList.h"
@@ -197,4 +197,4 @@ inline nsISupports* ToSupports(dom::SpeechRecognition* aRec) {
 
 }  // namespace mozilla
 
-#endif
+#endif // DOM_MEDIA_WEBSPEECH_RECOGNITION_SPEECHRECOGNITION_H_
--- dom/media/webspeech/recognition/test/FakeSpeechRecognitionService.h
+++ dom/media/webspeech/recognition/test/FakeSpeechRecognitionService.h
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_FakeSpeechRecognitionService_h
-#define mozilla_dom_FakeSpeechRecognitionService_h
+#ifndef DOM_MEDIA_WEBSPEECH_RECOGNITION_TEST_FAKESPEECHRECOGNITIONSERVICE_H_
+#define DOM_MEDIA_WEBSPEECH_RECOGNITION_TEST_FAKESPEECHRECOGNITIONSERVICE_H_
 
 #include "nsCOMPtr.h"
 #include "nsIObserver.h"
@@ -35,4 +35,4 @@ class FakeSpeechRecognitionService : public nsISpeechRecognitionService,
 
 }  // namespace mozilla
 
-#endif
+#endif // DOM_MEDIA_WEBSPEECH_RECOGNITION_TEST_FAKESPEECHRECOGNITIONSERVICE_H_
