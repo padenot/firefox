f650d192456a75a108a0f369bd3e1649c8458bc0
Paul Adenot <paul@paul.cx>
2025-12-08 15:15:57 +0100

    Bug 1940906 - Fix include guards in dom/media/webspeech. r?pehrsons
    Differential Revision: https://phabricator.services.mozilla.com/D275661

--- dom/media/webspeech/recognition/SpeechRecognitionResult.h
+++ dom/media/webspeech/recognition/SpeechRecognitionResult.h
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_SpeechRecognitionResult_h
-#define mozilla_dom_SpeechRecognitionResult_h
+#ifndef DOM_MEDIA_WEBSPEECH_RECOGNITION_SPEECHRECOGNITIONRESULT_H_
+#define DOM_MEDIA_WEBSPEECH_RECOGNITION_SPEECHRECOGNITIONRESULT_H_
 
 #include "SpeechRecognitionAlternative.h"
 #include "js/TypeDecls.h"
@@ -49,4 +49,4 @@ class SpeechRecognitionResult final : public nsISupports,
 
 }  // namespace mozilla::dom
 
-#endif
+#endif  // DOM_MEDIA_WEBSPEECH_RECOGNITION_SPEECHRECOGNITIONRESULT_H_
--- dom/media/webspeech/recognition/SpeechTrackListener.h
+++ dom/media/webspeech/recognition/SpeechTrackListener.h
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#ifndef mozilla_dom_SpeechStreamListener_h
-#define mozilla_dom_SpeechStreamListener_h
+#ifndef DOM_MEDIA_WEBSPEECH_RECOGNITION_SPEECHTRACKLISTENER_H_
+#define DOM_MEDIA_WEBSPEECH_RECOGNITION_SPEECHTRACKLISTENER_H_
 
 #include "AudioSegment.h"
 #include "MediaTrackGraph.h"
@@ -49,4 +49,4 @@ class SpeechTrackListener : public MediaTrackListener {
 }  // namespace dom
 }  // namespace mozilla
 
-#endif
+#endif  // DOM_MEDIA_WEBSPEECH_RECOGNITION_SPEECHTRACKLISTENER_H_
