53ea4daa8b0a7cb60fa301a767cc105668fbc445
Paul Adenot <paul@paul.cx>
2025-12-09 18:45:11 +0100

    Bug 1940906 - Introduce SpeechRecognitionBackend - Part 1 - Static methods. r?nika
    This patch starts filling in the blanks in SpeechRecognitionBackend. This
    object sits behind the SpeechRecogntion DOM object, and implements some light
    audio processing, and IPC communication.
    
    Audio processing is left for the next patch. This one implements the static
    methods, `available` and `install`, that call the IPDL methods of the same name.
    
    More importantly this patch implement a threading architecture that works for
    real-time audio, and DOM API, static and instance methods.
    
    The constraints are the following:
    - We're going to send method calls (available, install, start, stop, etc.) to
    the HWInference process, those come from the main thread (Web Speech API)
    - We're also going to need to send real-time audio to the same process (with a
    graduingly lower and lower packet size, to reduce end-to-end latency). This come
    from a hard-real-time thread (from our MediaTrackGraph)
    - Our IPC system requires all method calls to be on a single thread
    - The real-time audio thread can't do any system calls or unbounded operations
    (can't do IPC, locking, allocations, etc.)
    - We really don't want to bounce the real-time audio via the main thread for IPC
    or make it more busy if we can avoid it (it's already a nightmare in a typical
    media processing web app)
    - We can't affort scheduling hazards associated with using a shared thread (e.g.
    the shared background thread), because we want to have low and constant latency
    - There are both static and instance methods to implement
    
    The design is therefore the following:
    - A dedicated SpeechIPC thread is created, and shut down as soon as it's not
    needed. It is kept as a static member on the class
    - IPC only ever runs on this dedicated thread
    - Static method calls are dispatched to that thread, and the result is
    dispatched back to the main thread.
    - In the next patch, processed and packetized audio is dispatched to that
    thread as well
    - Whenever a Speech Recognition session is active, or while an available or
    install call is in flight, it is kept alive
    - Otherwise, we shut it down as soon as we can (session stop, result of
    `available` or `install`)
    
    The logic to manage those transient sessions (vs. long running ones) is
    encapsulated in a dedicated class.
    
    Differential Revision: https://phabricator.services.mozilla.com/D275653

--- dom/media/webspeech/recognition/SpeechRecognitionBackend.cpp
+++ dom/media/webspeech/recognition/SpeechRecognitionBackend.cpp
@@ -8,23 +8,74 @@
 
 #include "SpeechRecognition.h"
 #include "SpeechTrackListener.h"
-#include "mozilla/dom/AudioStreamTrack.h"
 #include "mozilla/AbstractThread.h"
 #include "mozilla/Assertions.h"
+#include "mozilla/dom/AudioStreamTrack.h"
 #include "mozilla/dom/ContentChild.h"
 #include "mozilla/dom/Promise.h"
+#include "mozilla/dom/SpeechRecognitionBinding.h"
+#include "mozilla/hwinference/HWInferenceManagerChild.h"
+#include "mozilla/ipc/MessageChannel.h"
+#include "mozilla/ipc/ProtocolUtils.h"
+#include "mozilla/SpeechRecognitionChild.h"
 
 namespace mozilla::dom {
 
+using namespace mozilla::ipc;
+
+StaticRefPtr<nsIThread> SpeechRecognitionBackend::sIPCThread;
+mozilla::EventTargetCapability<nsIThread>*
+    SpeechRecognitionBackend::sIPCCapability = nullptr;
+IPCThreadUserCounter SpeechRecognitionBackend::sIPCThreadUsers;
+StaticRefPtr<mozilla::hwinference::HWInferenceManagerChild>
+    SpeechRecognitionBackend::sHWInferenceChild;
+
+void IPCThreadUserCounter::Increment() { mCount++; }
+
+void IPCThreadUserCounter::Decrement() {
+  MOZ_ASSERT(mCount > 0);
+  mCount--;
+  if (!mCount) {
+    SpeechRecognitionBackend::StopIPCThreadIfPossible();
+  }
+}
+
+bool IPCThreadUserCounter::IsZero() const { return !mCount; }
+
+TransientSpeechRecognitionSession::TransientSpeechRecognitionSession()
+    : mChild(SpeechRecognitionBackend::sHWInferenceChild
+                 ->CreateSpeechRecognitionSession()) {
+  NS_DispatchToMainThread(NS_NewRunnableFunction(
+      "TransientSpeechRecognitionSession::Increment", []() {
+        AssertIsOnMainThread();
+        SpeechRecognitionBackend::sIPCThreadUsers.Increment();
+      }));
+}
+
+TransientSpeechRecognitionSession::~TransientSpeechRecognitionSession() {
+  SpeechRecognitionBackend::AssertOnIPCThread();
+  if (mChild) {
+    SpeechRecognitionChild::Send__delete__(mChild);
+    mChild = nullptr;
+  }
+  NS_DispatchToMainThread(NS_NewRunnableFunction(
+      "TransientSpeechRecognitionSession::Decrement", []() {
+        AssertIsOnMainThread();
+        SpeechRecognitionBackend::sIPCThreadUsers.Decrement();
+      }));
+}
+
 static LazyLogModule gSpeechRecognitionBackendLog("SpeechRecognitionBackend");
 
-#define LOG(...) \
-  MOZ_LOG_FMT(gSpeechRecognitionBackendLog, LogLevel::Debug, __VA_ARGS__)
-#define LOGV(...) \
-  MOZ_LOG_FMT(gSpeechRecognitionBackendLog, LogLevel::Verbose, __VA_ARGS__)
-#define LOGE(...) \
-  MOZ_LOG_FMT(gSpeechRecognitionBackendLog, LogLevel::Error, __VA_ARGS__)
-
+#define LOG(fmt, ...)                                                      \
+  MOZ_LOG_FMT(gSpeechRecognitionBackendLog, mozilla::LogLevel::Debug, fmt, \
+              ##__VA_ARGS__)
+#define LOGV(fmt, ...)                                                       \
+  MOZ_LOG_FMT(gSpeechRecognitionBackendLog, mozilla::LogLevel::Verbose, fmt, \
+              ##__VA_ARGS__)
+#define LOGE(fmt, ...)                                                     \
+  MOZ_LOG_FMT(gSpeechRecognitionBackendLog, mozilla::LogLevel::Error, fmt, \
+              ##__VA_ARGS__)
 
 SpeechRecognitionBackend::SpeechRecognitionBackend(
     SpeechRecognition* aParent, uint32_t aGraphRate, const nsString& aLanguage,
@@ -37,9 +88,12 @@ SpeechRecognitionBackend::~SpeechRecognitionBackend() {
   Abort();
 }
 
-nsresult SpeechRecognitionBackend::Start() { return NS_OK; }
+nsresult SpeechRecognitionBackend::Start() {
+  return NS_OK;
+}
 
-void SpeechRecognitionBackend::Stop() {}
+void SpeechRecognitionBackend::Stop() {
+}
 
 void SpeechRecognitionBackend::Abort() {
   AssertIsOnMainThread();
@@ -61,14 +115,307 @@ void SpeechRecognitionBackend::DataCallback(TrackTime aTime,
 
 void SpeechRecognitionBackend::NotifyTrackEnded() {}
 
-already_AddRefed<Promise> SpeechRecognitionBackend::Available(
-    nsIGlobalObject* aGlobal, const nsTArray<nsString>& aLanguages) {
-  return nullptr;
+/* static */
+nsCOMPtr<nsIThread> SpeechRecognitionBackend::GetOrCreateIPCThread() {
+  AssertIsOnMainThread();
+
+  if (!sIPCThread) {
+    nsCOMPtr<nsIThread> thread;
+    nsresult rv = NS_NewNamedThread("SpeechIPC", getter_AddRefs(thread));
+    if (NS_SUCCEEDED(rv)) {
+      sIPCThread = thread;
+      sIPCCapability = new EventTargetCapability<nsIThread>(sIPCThread);
+      LOG("Created shared IPC thread for speech recognition");
+    } else {
+      LOG("Failed to create shared IPC thread");
+      return nullptr;
+    }
+  }
+
+  return nsCOMPtr<nsIThread>(sIPCThread);
 }
 
+void SpeechRecognitionBackend::StopIPCThreadIfPossible() {
+  AssertIsOnMainThread();
+  if (sIPCThreadUsers.IsZero()) {
+    nsCOMPtr<nsIThread> ipcThread = sIPCThread.forget();
+    delete sIPCCapability;
+    sIPCCapability = nullptr;
+    ipcThread->Shutdown();
+    LOG("Stopped shared IPC thread");
+  }
+}
+
+/* static */
+void SpeechRecognitionBackend::AssertOnIPCThread() {
+  sIPCCapability->AssertOnCurrentThread();
+}
+
+/* static */
+template <typename Func>
+void SpeechRecognitionBackend::OnIPCThread(Func&& aFunc) {
+  MOZ_ASSERT(sIPCThread, "Programming error: IPC thread not initialized");
+  sIPCThread->Dispatch(NS_NewRunnableFunction(
+      "SpeechRecognitionBackend::OnIPCThread", std::forward<Func>(aFunc)));
+}
+
+/* static */
+already_AddRefed<Promise> SpeechRecognitionBackend::Available(
+    nsIGlobalObject* aGlobal, const nsTArray<nsString>& aLanguages) {
+  AssertIsOnMainThread();
+
+  if (!aGlobal) {
+    return nullptr;
+  }
+
+  ErrorResult rv;
+  RefPtr<Promise> promise = Promise::Create(aGlobal, rv);
+  if (rv.Failed()) {
+    return nullptr;
+  }
+
+  nsTArray<nsCString> languages;
+  for (const nsString& lang : aLanguages) {
+    languages.AppendElement(NS_ConvertUTF16toUTF8(lang));
+  }
+  if (languages.IsEmpty()) {
+    languages.AppendElement("en-US"_ns);
+  }
+
+  LOG("SpeechRecognitionBackend::Available - Starting availability check for "
+      "{} languages",
+      languages.Length());
+
+  for (const auto& lang : languages) {
+    LOG("SpeechRecognitionBackend::Available - Language requested: {}",
+        lang.get());
+  }
+
+  nsMainThreadPtrHandle<Promise> promiseHandle(
+      MakeAndAddRef<nsMainThreadPtrHolder<Promise>>(
+          "SpeechRecognitionBackend::Available", promise));
+
+  EnsureIPC()->Then(
+      GetCurrentSerialEventTarget(), __func__,
+      [promiseHandle, languages = std::move(languages)](bool aSuccess) mutable {
+        AssertIsOnMainThread();
+        if (!aSuccess) {
+          LOG("SpeechRecognitionBackend::Available - Failed to initialize IPC");
+          promiseHandle->MaybeResolve(AvailabilityStatus::Unavailable);
+          return;
+        }
+
+        OnIPCThread([promiseHandle,
+                     languages = std::move(languages)]() mutable {
+          LOG("SpeechRecognitionBackend::Available - Connection ready, "
+              "checking model availability");
+
+          auto session = MakeUnique<TransientSpeechRecognitionSession>();
+
+          if (!session->get()) {
+            LOG("SpeechRecognitionBackend::Available - Failed to create speech "
+                "recognition session");
+            NS_DispatchToMainThread(NS_NewRunnableFunction(
+                "SpeechRecognitionBackend::ResolveUnavailable",
+                [promiseHandle]() {
+                  promiseHandle->MaybeResolve(AvailabilityStatus::Unavailable);
+                }));
+            return;
+          }
+
+          session->get()->SendIsModelAvailable(languages)->Then(
+              GetCurrentSerialEventTarget(), __func__,
+              [promiseHandle, session = std::move(session)](bool available) {
+                LOG("SpeechRecognitionBackend::Available - Received response: "
+                    "{}",
+                    available ? "true" : "false");
+                NS_DispatchToMainThread(NS_NewRunnableFunction(
+                    "SpeechRecognitionBackend::ResolveAvailable",
+                    [promiseHandle, available]() {
+                      if (available) {
+                        promiseHandle->MaybeResolve(
+                            AvailabilityStatus::Available);
+                      } else {
+                        promiseHandle->MaybeResolve(
+                            AvailabilityStatus::Downloadable);
+                      }
+                    }));
+              },
+              [promiseHandle,
+               session = std::move(session)](ResponseRejectReason reason) {
+                LOG("SpeechRecognitionBackend::Available failed: {}",
+                    static_cast<int>(reason));
+                NS_DispatchToMainThread(NS_NewRunnableFunction(
+                    "SpeechRecognitionBackend::ResolveUnavailable",
+                    [promiseHandle]() {
+                      promiseHandle->MaybeResolve(
+                          AvailabilityStatus::Unavailable);
+                    }));
+              });
+        });
+      },
+      [promiseHandle](nsresult aError) {
+        LOG("SpeechRecognitionBackend::Available - IPC initialization failed");
+        promiseHandle->MaybeResolve(AvailabilityStatus::Unavailable);
+      });
+
+  return promise.forget();
+}
+
+/* static */
+RefPtr<GenericPromise> SpeechRecognitionBackend::EnsureIPC() {
+  AssertIsOnMainThread();
+
+  // This ensures IPC has been setup. It requires two things:
+  //
+  // - a thread, on which all IPC calls are made
+  // - effectively setting up the connection.
+  //
+  // Since sHWInferenceChild is SpeechIPC thread only, and this isn't
+  // performance sensitive by any mean, we do this asynchronously. If not
+  // set-up, the connection is established (on the main thread), then
+  // sHWInferenceChild is initialized, and the promise is finally resolved.
+
+  nsCOMPtr<nsIThread> ipcThread = GetOrCreateIPCThread();
+  if (!ipcThread) {
+    LOG("EnsureIPC - Failed to get IPC thread");
+    return mozilla::GenericPromise::CreateAndReject(NS_ERROR_FAILURE, __func__);
+  }
+
+  return InvokeAsync(ipcThread, __func__, []() -> RefPtr<GenericPromise> {
+    AssertOnIPCThread();
+    if (sHWInferenceChild && sHWInferenceChild->CanSend()) {
+      LOG("EnsureIPC - Connection already established (checked on IPC thread)");
+      return GenericPromise::CreateAndResolve(true, __func__);
+    }
+
+    return InvokeAsync(
+        GetMainThreadSerialEventTarget(), __func__,
+        []() -> RefPtr<GenericPromise> {
+          AssertIsOnMainThread();
+
+          ContentChild* contentChild = ContentChild::GetSingleton();
+          if (!contentChild) {
+            LOG("EnsureIPC - No ContentChild available");
+            return GenericPromise::CreateAndReject(NS_ERROR_FAILURE, __func__);
+          }
+
+          LOG("EnsureIPC - Creating endpoint pair");
+
+          mozilla::ipc::Endpoint<hwinference::PHWInferenceManagerParent> parentEp;
+          mozilla::ipc::Endpoint<hwinference::PHWInferenceManagerChild> childEp;
+
+          MOZ_ALWAYS_SUCCEEDS(
+              hwinference::PHWInferenceManager::CreateEndpoints(&parentEp, &childEp));
+          DebugOnly<bool> ok = contentChild->SendRequestHWInferenceConnection(
+              std::move(parentEp));
+          MOZ_ASSERT(ok);
+
+          return InvokeAsync(
+              sIPCThread, __func__,
+              [childEp =
+                   std::move(childEp)]() mutable -> RefPtr<GenericPromise> {
+                AssertOnIPCThread();
+
+                LOG("EnsureIPC - Opening connection on the SpeechIPC thread");
+                [&]() MOZ_NO_THREAD_SAFETY_ANALYSIS {
+                  mozilla::hwinference::HWInferenceManagerChild::OpenForProcess(std::move(childEp));
+                }();
+                sHWInferenceChild = mozilla::hwinference::HWInferenceManagerChild::GetSingleton();
+
+                if (sHWInferenceChild->CanSend()) {
+                  LOG("EnsureIPC - Connection established");
+                  return GenericPromise::CreateAndResolve(true, __func__);
+                }
+                LOG("EnsureIPC - Failed to establish");
+                return GenericPromise::CreateAndReject(NS_ERROR_FAILURE,
+                                                       __func__);
+              });
+        });
+  });
+}
+
+/* static */
 already_AddRefed<Promise> SpeechRecognitionBackend::Install(
     nsIGlobalObject* aGlobal, const nsTArray<nsString>& aLanguages) {
-  return nullptr;
+  AssertIsOnMainThread();
+
+  if (!aGlobal) {
+    return nullptr;
+  }
+
+  ErrorResult rv;
+  RefPtr<Promise> promise = Promise::Create(aGlobal, rv);
+  if (rv.Failed()) {
+    return nullptr;
+  }
+
+  if (aLanguages.IsEmpty()) {
+    promise->MaybeResolve(false);
+    return promise.forget();
+  }
+
+  nsTArray<nsCString> languages;
+  for (const nsString& lang : aLanguages) {
+    languages.AppendElement(NS_ConvertUTF16toUTF8(lang));
+  }
+
+  LOG("SpeechRecognitionBackend::Install - Starting install for {} languages",
+      languages.Length());
+
+  nsMainThreadPtrHandle<Promise> promiseHandle(
+      MakeAndAddRef<nsMainThreadPtrHolder<Promise>>(
+          "SpeechRecognitionBackend::Install", promise));
+
+  EnsureIPC()->Then(
+      GetCurrentSerialEventTarget(), __func__,
+      [promiseHandle, languages = std::move(languages)](bool aSuccess) mutable {
+        if (!aSuccess) {
+          LOG("SpeechRecognitionBackend::Install - Failed to initialize IPC");
+          promiseHandle->MaybeResolve(false);
+          return;
+        }
+
+        OnIPCThread([promiseHandle,
+                     languages = std::move(languages)]() mutable {
+          LOG("SpeechRecognitionBackend::Install - Connection ready, starting "
+              "model installation");
+
+          auto session = MakeUnique<TransientSpeechRecognitionSession>();
+
+          session->get()
+              ->SendInstallModels(std::move(languages))
+              ->Then(
+                  GetCurrentSerialEventTarget(), __func__,
+                  [promiseHandle, session = std::move(session)](bool success) {
+                    LOG("SpeechRecognitionBackend::Install - Install "
+                        "completed: {}",
+                        success ? "success" : "failed");
+                    NS_DispatchToMainThread(NS_NewRunnableFunction(
+                        "SpeechRecognitionBackend::ResolveInstall",
+                        [promiseHandle, success]() {
+                          promiseHandle->MaybeResolve(success);
+                        }));
+                  },
+                  [promiseHandle,
+                   session = std::move(session)](ResponseRejectReason aReason) {
+                    LOG("SpeechRecognitionBackend::Install - Install failed "
+                        "with reason: {}",
+                        static_cast<int>(aReason));
+                    NS_DispatchToMainThread(NS_NewRunnableFunction(
+                        "SpeechRecognitionBackend::ResolveInstallFailed",
+                        [promiseHandle]() {
+                          promiseHandle->MaybeResolve(false);
+                        }));
+                  });
+        });
+      },
+      [promiseHandle](nsresult aError) {
+        LOG("SpeechRecognitionBackend::Install - IPC initialization failed");
+        promiseHandle->MaybeResolve(false);
+      });
+
+  return promise.forget();
 }
 
 }  // namespace mozilla::dom
