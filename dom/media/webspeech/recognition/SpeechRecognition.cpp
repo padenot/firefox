607245705e89c249853a33e23b39cb44859f74ab
Paul Adenot <paul@paul.cx>
2026-06-29 17:24:22 +0000

    Bug 1940906 - Rename SpeechRecognitionError to SpeechRecognitionErrorEvent. r?#media-playback-reviewers
    The interface was renamed to SpeechRecognitionErrorEvent in the Web Speech
    API spec; align the Gecko interface, its binding, and the (now non-nullable)
    message attribute / required init members accordingly.
    
    Differential Revision: https://phabricator.services.mozilla.com/D311102

--- dom/media/webspeech/recognition/SpeechRecognition.cpp
+++ dom/media/webspeech/recognition/SpeechRecognition.cpp
@@ -29,7 +29,7 @@
 #include "mozilla/dom/PromiseNativeHandler.h"
 #include "mozilla/dom/RootedDictionary.h"
 #include "mozilla/dom/SpeechGrammar.h"
-#include "mozilla/dom/SpeechRecognitionError.h"
+#include "mozilla/dom/SpeechRecognitionErrorEvent.h"
 #include "mozilla/dom/SpeechRecognitionEvent.h"
 #include "mozilla/dom/SpeechRecognitionPhrase.h"
 #include "mozilla/intl/Locale.h"
@@ -745,8 +745,8 @@ void SpeechRecognition::DispatchError(SpeechRecognitionErrorCode aErrorCode,
                                       const nsACString& aMessage) {
   MOZ_ASSERT(NS_IsMainThread(), "DispatchError must be on main thread");
 
-  RefPtr<SpeechRecognitionError> srError =
-      new SpeechRecognitionError(nullptr, nullptr, nullptr);
+  RefPtr<SpeechRecognitionErrorEvent> srError =
+      new SpeechRecognitionErrorEvent(nullptr, nullptr, nullptr);
 
   srError->InitSpeechRecognitionError(u"error"_ns, true, false, aErrorCode,
                                       aMessage);
@@ -827,8 +827,8 @@ void SpeechRecognition::HandleRecognitionErrorFromBackend(
     return;
   }
 
-  RefPtr<SpeechRecognitionError> srError =
-      new SpeechRecognitionError(nullptr, nullptr, nullptr);
+  RefPtr<SpeechRecognitionErrorEvent> srError =
+      new SpeechRecognitionErrorEvent(nullptr, nullptr, nullptr);
 
   // Map backend errors to appropriate error codes
   SpeechRecognitionErrorCode errorCode = SpeechRecognitionErrorCode::Network;
